18fb8caf20dc6ecf3051a109a51564b262f28eac
SilenMe <vivek2vks@gmailcom>
2020-09-27 18:46:27 +0530

    set css and javascript dir and implemented random jinja

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/') #base url(just domain)
+all_posts=[
+{
+	'title':'Post 1',
+	'content':'Post 1 content',
+	'author':'Sahani'
+},
+{
+	'title':'Post 2',
+	'content':'Post 2 content'
+}]
 
-def hello():
-	return "Hello world"
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+	return render_template('posts.html',posts=all_posts) # now we can use this Post variable in templates or wherever we want
+ 
+@app.route('/home/<string:name>')#base url(just domain)
+
+def hello(name):
+	return "Hello " + name
 
 if __name__ == '__main__':
 	app.run(debug=True)
