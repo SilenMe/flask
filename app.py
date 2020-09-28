320ddf0ef081680329ae3b9e430ba30333a0bd37
SilenMe <vivek2vks@gmailcom>
2020-09-28 10:21:38 +0530

    implemented crud

--- app.py
+++ app.py
@@ -1,30 +1,68 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app=Flask(__name__)
 
-all_posts=[
-{
-	'title':'Post 1',
-	'content':'Post 1 content',
-	'author':'Sahani'
-},
-{
-	'title':'Post 2',
-	'content':'Post 2 content'
-}]
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'  # for devlopmemnt
+db = SQLAlchemy(app)
+
+
+class BlogPost(db.Model):
+    # """docstring for BlogPost"""
+    # def __init__(self, arg):
+    # 	super(BlogPost, self).__init__()
+    # 	self.arg = arg
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+
+    def __repr__(self):  # to print the submitted blog instantly, function must return a String otherwise error will be thrown
+        return 'Blog Post  ' + str(self.id)
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/posts')
+
+@app.route('/posts',methods=['GET','POST']) #gt and post to skip error
 def posts():
-	return render_template('posts.html',posts=all_posts) # now we can use this Post variable in templates or wherever we want
- 
-@app.route('/home/<string:name>')#base url(just domain)
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author=request.form['author']
+        new_post = BlogPost(title=post_title,content=post_content,author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
 
-def hello(name):
-	return "Hello " + name
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>',methods=['GET','POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method=='POST':
+        
+        post.title=request.form['title']
+        post.author=request.form['author']
+        post.content=request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# about the app
+a blog app using flask
+
+## fill the requirements of requirments.txt then run the app.py
+
+
+
+if you want to use your database then run the following commands in pyhton shell after deleting my db:
+>>>from app import db #db is created in app.py
+>>>db.create_all()
+
+
+
